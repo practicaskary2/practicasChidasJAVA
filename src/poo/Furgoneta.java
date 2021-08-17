4a38871bcd217bfc7a978c0e477e9ca67fb41f1c
practicaskary2 <practicasgithub2@gmail.com>
2021-08-17 18:59:44 -0500

    Herencia -> extends, super()

---
+++ src/poo/Furgoneta.java
@@ -0,0 +1,17 @@
+package poo;
+
+
+//Hereda de la clase Coche
+public class Furgoneta extends Coche {
+
+	private int capacidad_carga;
+	private int plazas_extras;
+	
+	public Furgoneta(int capacidad_carga, int plazas_extras  ) {
+		super(); //Llamar al método constructor de la clases padre, en este caso Coche
+		
+		this.capacidad_carga = capacidad_carga;
+		this.plazas_extras = plazas_extras;
+		
+	}
+}
