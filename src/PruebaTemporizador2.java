8459d87fde3b1e48a9d07f995042cd7ad15e38f6
practicaskary2 <practicasgithub2@gmail.com>
2021-08-25 19:47:48 -0500

    Clases internas locales -> Son clases dentro de un metodo

--- src/PruebaTemporizador2.java
+++ src/PruebaTemporizador2.java
@@ -12,8 +12,8 @@ public class PruebaTemporizador2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Reloj miReloj = new Reloj(3000,true);
-		miReloj.enMarcha();
+		Reloj miReloj = new Reloj();
+		miReloj.enMarcha(3000,true);
 		JOptionPane.showMessageDialog(null, "Pulsa OK para terminar el programa");
 		System.exit(0);
 
@@ -22,38 +22,29 @@ public class PruebaTemporizador2 {
 }
 
 class Reloj{
-	private int intervalo;
-	private boolean sonido;
 	
-	public Reloj(int intervalo,boolean sonido) {
-		this.intervalo =  intervalo;
-		this.sonido = sonido;
-	}
-	
-	public void enMarcha() {
+	public void enMarcha(int intervalo, boolean sonido) {
+		
+		//CLASE INTERNA LOCAL
+		//No puede tener modificador de acceso
+		//No es visible por la clase externa en este caso Reloj
+		class DameLaHora2 implements ActionListener{
+			
+			//Método obligatorio por implementar interfaz ActionListener
+			public void actionPerformed(ActionEvent evento) {
+				
+				Date ahora = new Date();
+				System.out.println("Muestro la hora cada 3 segundos " + ahora);
+				
+				if(sonido) {
+					Toolkit.getDefaultToolkit().beep();
+				}
+			}
+		}
+		
 		ActionListener oyente = new DameLaHora2();
 		Timer miTemporizador = new Timer(intervalo,oyente);
 		miTemporizador.start();
 		
-	}
-	
-	
-	
-	//CLASE INTERNA
-	//Solo las clases internas pueden hacer uso del modificador private
-	//La ventaja de usar esta clase interna es que tiene acceso a las propiedades de la clase que la engloba, por ejemplo *private boolean sonido;* en este caso
-	//Te evitas crear el setter y getter
-	private class DameLaHora2 implements ActionListener{
-		
-		//Método obligatorio por implementar interfaz ActionListener
-		public void actionPerformed(ActionEvent evento) {
-			
-			Date ahora = new Date();
-			System.out.println("Muestro la hora cada 3 segundos " + ahora);
-			
-			if(sonido) {
-				Toolkit.getDefaultToolkit().beep();
-			}
-		}
 	}
 }
