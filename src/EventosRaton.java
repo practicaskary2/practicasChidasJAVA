d4d65d31f7c0a104447ec20b6c27dac99f961722
practicaskary2 <practicasgithub2@gmail.com>
2021-09-01 06:42:38 -0500

    Eventos:Eventos de teclado. Implementar dos interfaces -> MouseListener,MouseMotionListener

--- src/EventosRaton.java
+++ src/EventosRaton.java
@@ -1,5 +1,7 @@
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
 import javax.swing.JFrame;
 
 public class EventosRaton {
@@ -23,40 +25,62 @@ class MarcoRaton extends JFrame{
 		
 		EventosDeRaton eventoRaton = new EventosDeRaton();
 		addMouseListener(eventoRaton);
+		addMouseMotionListener(eventoRaton);
 	}
 }
 
-class EventosDeRaton implements MouseListener{  //En caso de no querer declarar todos los métodos se puede usar la clase MouseAdapter -> extends MouseAdapter
+class EventosDeRaton implements MouseListener,MouseMotionListener{  //En caso de no querer declarar todos los métodos se puede usar la clase MouseAdapter -> extends MouseAdapter
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("Hiciste click con el ratón");
+		//System.out.println("Hiciste click con el ratón");
+		//System.out.println("Cordenada X: " + e.getX() + " Cordenada Y: " + e.getY());
+		//System.out.println("Hiciste click " + e.getClickCount() + " veces");
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
+		//System.out.println("Acabas de presionar");
 		
-		System.out.println("Acabas de presionar");
+		if(e.getModifiersEx() == MouseEvent.BUTTON1_DOWN_MASK) {
+			System.out.println("Pulsaste el botón izquierdo del mouse");
+		}else if(e.getModifiersEx() == MouseEvent.BUTTON2_DOWN_MASK) {
+			System.out.println("Pulsaste la rueda del mouse");
+		}else if(e.getModifiersEx() == MouseEvent.BUTTON3_DOWN_MASK) {
+			System.out.println("Pulsaste el botón derecho del mouse");
+		}
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("Acabas de soltar");
+		//System.out.println("Acabas de soltar");
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("El ratón está por encima de la ventana");
+		//System.out.println("El ratón está por encima de la ventana");
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("Saliste del marco de la ventana");
+		//System.out.println("Saliste del marco de la ventana");
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("Estás moviendo el mouse");
 	}
 	
 }
